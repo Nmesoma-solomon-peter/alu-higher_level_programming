b3e824ef03a0c344d2b930c821d452e0dbf93d3f
nmesoma <nmesomasolo2809@gmail.com>
2024-03-24 01:17:40 -0700

    task total replace

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
-"""
+"""Fetches https://intranet.hbtn.io/status"""
+
 
 if __name__ == "__main__":
     import urllib.request
-    from urllib.request import HTTPBasicAuth
-    
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
