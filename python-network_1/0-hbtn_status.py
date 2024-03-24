a316e889ad83fd1edfc82f56798f260c85cf17b4
nmesoma <nmesomasolo2809@gmail.com>
2024-03-24 02:28:24 -0700

    task 0 try againg

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,11 +4,9 @@
 
 if __name__ == "__main__":
     import urllib.request
-    
-    username = "ed757ca7f0b2"
-    password = "1251b956dca70ce1b90a"
-    with urllib.request.urlopen('https://intranet.hbtn.io/status', auth=HTTPBasicAuth(username, password)) as response: 
-        https://alu-intranet.hbtn.io/statushtml = response.read()
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
