d80547fb41432fa0e3fe37a917d2f9e77b201e82
nmesoma <nmesomasolo2809@gmail.com>
2024-03-24 02:03:28 -0700

    added password and username

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,8 +4,11 @@
 
 if __name__ == "__main__":
     import urllib.request
+    
+    username = "ed757ca7f0b2"
+    password = "1251b956dca70ce1b90a"
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status', auth=HTTPBasicAuth(username, password)) as response: 
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
